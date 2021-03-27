63a571575ca637fa0669ddc46af8b2fadbaee121
Will Walles <willwalles@Wills-MacBook-Pro.local>
2021-03-26 20:03:22 -0500

    finished code to create a table for date enquiries

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,11 +3,11 @@ var tableData = data;
 
 console.log(tableData);
 
-// References to tbody, input field, and button
+// References to inputText, buttion, tbody
 
 var inputText = d3.select(".form-control");
 var button = d3.select("#filter-btn");
-
+var tbody = d3.select("tbody");
 
 // create function to filter by date
 // function filterByDate(sighting) {
@@ -23,14 +23,36 @@ var button = d3.select("#filter-btn");
 
 // start of function to take in input text and action on 'filter button click'
 inputText.on("change", function() {
+    // select the input text
     var queryText = d3.event.target.value;
-    // console.log(newText);
+   
+    // fuction for on button click
     button.on("click", function() {
+        
+        // display date entered
         console.log(queryText);
+        
+        // Clear contents of tbody
+        tbody = tbody.text('')
+
+        // Use function to filter data based off of query text
         function filterByDate(sighting) {
             return sighting.datetime === queryText;}
         var filteredData = tableData.filter(filterByDate);
         console.log(filteredData);
+        // Get length of filtered data for table
+        filterDataLength = filteredData.length;
+        console.log(filterDataLength);
+
+        // Start building table
+        filteredData.forEach((sighting) => {
+            var row = tbody.append("tr");
+            Object.entries(sighting).forEach(([key, value]) => {
+                var cell = row.append("td");
+                cell.text(value);
+            });
+        });
+    
     });
   });
 
